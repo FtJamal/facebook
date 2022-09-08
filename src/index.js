6312360227825ebacd32a3e7203aa2eec1a71f51
farya <ftjamal2017@gmail.com>
2022-09-08 21:04:15 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import postImg from "./img/nature.jpg";
+import quoteImg from "./img/quote.jpg";
+import pakDay from "./img/pak.jpg";
+import profileImg from "./img/profile.jpg";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const MediaCard =
+  ({ profile, name, date, text, imageUrl, likes, comments, shares }) => (
+    <div class="body">
+      <div class="header">
+        <img class="profile" src={profile} alt="" />
+        <div>
+          <p class="name">{name}</p>
+          <p class="date">{date}</p>
+        </div>
+      </div>
+
+      <p>
+        {text}
+      </p>
+
+      <img class="post" src={imageUrl} alt="" />
+
+      <div class="a">
+        <span>{likes}</span>   <span>{comments}</span>   <span>{shares}</span>
+      </div>
+
+      <div class="button">
+        <button>Like</button>   <button>Comment</button>   <button>Share</button>
+      </div>
+
+    </div>
+  )
+
+const MediaPage = () => (
+  <div>
+    <MediaCard
+      profile={profileImg}
+      name="Farya Jamal"
+      date="8 Sep 2022"
+      text="Try,try never say why"
+      imageUrl={postImg}
+      likes={"250 likes"}
+      comments={"70 comments"}
+      shares={"100 shares"}
+    />
+
+    <MediaCard
+      profile={profileImg}
+      name="Farya Jamal"
+      date="7 Sep 2022"
+      text="Stay Blessed!"
+      imageUrl={quoteImg}
+      likes={"100 likes"}
+      comments={"150 comments"}
+      shares={"90 shares"}
+    />
+
+    <MediaCard
+      profile={profileImg}
+      name="Farya Jamal"
+      date="14 aug 2022"
+      text="Pakistan Zindabaad!"
+      imageUrl={pakDay}
+      likes={"200 likes"}
+      comments={"100 comments"}
+      shares={"50 shares"}
+    />
+  </div>
+)
+
+ReactDOM.render(<MediaPage />, document.querySelector("#root"));
